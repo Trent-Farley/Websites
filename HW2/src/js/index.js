2e359acbf64b1cd75894fde3261b653c3e08b87c
Trent Farley <trent.a.farley@gmail.com>
2020-10-06 10:27:08 -0700

    finished scaffolding, also added an append function for easy appending to the DOM

--- HW2/src/js/index.js
+++ HW2/src/js/index.js
@@ -1,1 +1,13 @@
-const root = $('container')
+const root = $(".container");
+let append = (el, content, classname,attribute) => {
+    if (classname === undefined && attribute === undefined) {
+        root.append(`<${el}> ${content} <${el}>`)
+    } else if(attribute === undefined) {
+        root.append(`<${el} class="${classname}"> ${content} <${el}>`)
+    } else{
+        root.append(`<${el} class="${classname}" ${attribute}> ${content} <${el}>`)
+    }
+};
+
+
+
