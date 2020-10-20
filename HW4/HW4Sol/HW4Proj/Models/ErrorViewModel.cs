bd76c8e2071da925849232078c2e707a30bafb5d
Trent Farley <trent.a.farley@gmail.com>
2020-10-20 10:26:45 -0700

    Created ASP.NET Core application as instructed, swtitching branches now

---
+++ HW4/HW4Sol/HW4Proj/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace HW4Proj.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
