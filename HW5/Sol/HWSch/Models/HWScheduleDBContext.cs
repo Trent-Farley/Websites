729938aa778b93e8846e180374b13743b5722bec
Trent Farley <trent.a.farley@gmail.com>
2020-10-28 18:43:57 -0700

    Added the DB, testing it now

---
+++ HW5/Sol/HWSch/Models/HWScheduleDBContext.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+namespace HWSch.Models
+{
+    public partial class HWScheduleDBContext : DbContext
+    {
+        public HWScheduleDBContext()
+        {
+        }
+
+        public HWScheduleDBContext(DbContextOptions<HWScheduleDBContext> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<Homework> Homeworks { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                optionsBuilder.UseSqlServer("Name=HWSchedulerConnection");
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            OnModelCreatingPartial(modelBuilder);
+        }
+
+        partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+    }
+}
