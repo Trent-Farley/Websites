fa681b6ec30e9efdd254c6b3bcfe341fa4cb34e5
Trent Farley <trent.a.farley@gmail.com>
2020-11-12 14:43:43 -0800

    AJAX requests for repo and user are working, Now I need to get commits showing

---
+++ HW7/Sol/Proj/wwwroot/js/index.js
@@ -0,0 +1,43 @@
+﻿$(document).ready(() => {
+    getUser();
+    getRepos();
+})
+
+let getUser = async () => {
+    await $.getJSON("/api/User", (data) => {
+        $("#userImg").attr("src", data.img);
+        $("#userName").text(data.name);
+        $("#userLocation").text(data.location);
+        $("#userEmail").text(data.email);
+        $("#userCompany").text(data.company);
+    }).fail(() => { console.log("failed to get json in user") });
+    $("#content").css("display", "block");
+}
+
+let getRepos = async () => {
+    await $.getJSON("/api/Repos", (data) => {
+        data.forEach(repo => addRepo(repo))
+    });
+}
+
+let addRepo = (repo) => {
+    let repoHTML = `
+    <div class="card">
+        <div class="card-body">
+        <div class="row">
+            <div class="col">
+                <img  src="${repo.ownerAvatarUrl}" alt="Profile photo" class="img-thumbnail" />
+            </div>
+            <div class="col">
+                <h6 class="card-title">${repo.name}</h6>
+                <p class="card-text">
+                    Owner login: ${repo.ownerLogin}<br>
+                    Last Push: ${repo.pushedAt}
+                </p>
+            </div>
+        </div>
+        </div>
+    </div>
+    `
+    $("#repos").append(repoHTML);
+}
