e97bc55752abe67461e79276cee1483a296127e4
Trent Farley <trent.a.farley@gmail.com>
2020-11-21 19:21:58 -0800

    Project eight is set up, switching branches to complete feature 1

---
+++ HW8/HWSchV2/HWSchV2/Models/HWSch.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+#nullable disable
+
+namespace HWSchV2.Models
+{
+    public partial class HWSch : DbContext
+    {
+        public HWSch()
+        {
+        }
+
+        public HWSch(DbContextOptions<HWSch> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<Course> Courses { get; set; }
+        public virtual DbSet<Detail> Details { get; set; }
+        public virtual DbSet<Homework> Homeworks { get; set; }
+        public virtual DbSet<Tag> Tags { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                optionsBuilder.UseSqlServer("Name=ConnectionStrings:HWSchv2");
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Homework>(entity =>
+            {
+                entity.Property(e => e.Done).HasDefaultValueSql("((0))");
+
+                entity.HasOne(d => d.Class)
+                    .WithMany(p => p.Homework)
+                    .HasForeignKey(d => d.ClassId)
+                    .HasConstraintName("HW_FK_COURSE");
+
+                entity.HasOne(d => d.Info)
+                    .WithMany(p => p.Homework)
+                    .HasForeignKey(d => d.InfoId)
+                    .HasConstraintName("HW_FK_DETAIL");
+
+                entity.HasOne(d => d.Line)
+                    .WithMany(p => p.Homework)
+                    .HasForeignKey(d => d.LineId)
+                    .HasConstraintName("HW_FK_TAG");
+            });
+
+            OnModelCreatingPartial(modelBuilder);
+        }
+
+        partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+    }
+}
---
+++ HW8/HWSchV2/HWSchV2/Models/Tag.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using Microsoft.EntityFrameworkCore;
+
+#nullable disable
+
+namespace HWSchV2.Models
+{
+    [Table("Tag")]
+    public partial class Tag
+    {
+        public Tag()
+        {
+            Homework = new HashSet<Homework>();
+        }
+
+        [Key]
+        public int Id { get; set; }
+        [StringLength(40)]
+        public string Tagname { get; set; }
+
+        [InverseProperty("Line")]
+        public virtual ICollection<Homework> Homework { get; set; }
+    }
+}
