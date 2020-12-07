87684f5ede7ecdfe4fbba89e29f14213d626dd6a
Trent Farley <trent.a.farley@gmail.com>
2020-12-07 14:08:36 -0800

    Chore: c-E controller and views m-startup c-Models from db and context

---
+++ Final/Expeditions/ViewModels/Hike.cs
@@ -0,0 +1,14 @@
+﻿using Expeditions.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Expeditions.ViewModels
+{
+    public class Hike
+    {
+        public IEnumerable<Expedition> Hikes { get; set; }
+    }
+}
---
+++ Final/Expeditions/wwwroot/js/site.js
@@ -0,0 +1,12 @@
+﻿// Write your JavaScript code.
+
+// This will set up a timer.  It will invoke the execute function every 5 seconds
+$(document).ready(function () { window.setInterval(execute, 5000) });
+let count = 0;
+function execute() {
+    console.log('Running execute function');
+    if (count % 2) {
+        console.clear();
+        console.warn('Running execute');
+    }
+}
